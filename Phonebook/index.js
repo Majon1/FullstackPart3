173b01eefc00e22bb9018116300dae7cde03434d
Majon1 <mathilda.nynas@gmail.com>
2020-10-08 08:15:56 +0300

    ex3.5 and 3.6 done

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -25,29 +25,47 @@ let persons = [
         number: '39-23-6423122'
     }
 ]
-const generateId = () => {
+
+  const generateId = () => {
     const maxId = persons.length > 0
-      ? Math.random() * 20
+      ? Math.random(...persons.map(n => n.id))
       : 0
     return maxId + 1
   }
-
-app.post('/api/persons', (request, response) => {
+  
+  app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    /*if(!body.name || !body.number || ) {
-        return response.status(400).json({
-            error: 'name missing'
+  
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
         })
-    }*/
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
+      }
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+    if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase()))
+    {
+        return response.status(400).json({ 
+            error: body.name + ' already in phonebook!' 
+          })
     }
+    
+
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+      
+    }
+  
     persons = persons.concat(person)
+  
     response.json(person)
   })
+
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
