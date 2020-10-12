ef7214c6c5367316be246f55baab58b97da65879
Majon1 <mathilda.nynas@gmail.com>
2020-10-12 15:22:59 +0300

    build added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
  morgan.token('json', function(req, res) { 
   return JSON.stringify({
