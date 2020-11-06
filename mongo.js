d8e0386cd9fa8d362f93f32d98d199e8a8df44cd
Majon1 <mathilda.nynas@gmail.com>
2020-11-06 10:41:03 +0200

    part3 finally working

--- mongo.js
+++ mongo.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
+const password = process.argv[2]
+const names = process.argv[3]
+const numbers = process.argv[4]
+const url = 
+`mongodb+srv://fullstack:${password}@cluster0.al01h.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
 
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
-
-const password = process.argv[2]
-const names = process.argv[3]
-const numbers = process.argv[4]
-const url = process.env.MONGODB_URI
-
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
   id: Number,
@@ -21,19 +21,17 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-//add new persons 
 const person = new Person({
   name: names,
   number: numbers,
 })
 
-person.save().then(result => {
+person.save().then(() => {
   console.log('added ' + person.name + ' number ' + person.number + ' to phonebook')
-  mongoose.connection.close()
-})
+  mongoose.connection.close()})
 
 if (process.argv.length < 4) {
-  //show all persons
+  
   Person
     .find({})
     .then(result => {
